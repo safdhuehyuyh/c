c5ca182c982886ba91bd9e009ae4150a706b8239
Samyak Bhoyar <78425159+safdhuehyuyh@users.noreply.github.com>
2021-07-03 13:46:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ function draw(){
         objectDetector.detect(video, gotResult);
         for(i = 0; i < objects.length; i++) {
             document.getElementById("status").innerHTML = "Status : Object Detected";
-            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+            document.getElementById("number_of_object").innerHTML = "Number of objects detected are : " + objects.length;
             fill('#FF0000');
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
-            noFill()
-            stroke('#FF0000')
+            noFill();
+            stroke('#FF0000');
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
          
         }
